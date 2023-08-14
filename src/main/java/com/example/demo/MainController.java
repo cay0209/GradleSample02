7dc6836fcb1dfc3bc7a12710a55ece760f8ca0d1
ayayay <cay0209@gmail.com>
2023-08-14 12:32:14 +0900

    add index

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -5,13 +5,15 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("")
 public class MainController {
 
 	
-	@GetMapping({"", "/"})
+	@GetMapping("")
 	public ResponseEntity<?> index(){
 		Map<String, Object> map = new HashMap<>();
 		map.put("key", "3333");
